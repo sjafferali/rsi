b72991dd8b47dfda2ec7c733a67ebc881bfb00f2
Samir Jafferali <sami8562@mnq0gsf4jl.rackspace.corp>
2015-03-24 20:25:35 -0500

    Created sub for indented output

--- rsi.sh
+++ rsi.sh
@@ -8,6 +8,10 @@ print_warn () {
 echo "[WARN] * $1"
 }
 
+print_sub () {
+echo -e "\t - $1"
+}
+
 print_info "`cat /etc/redhat-release | head -1`"
 cpus=`cat /proc/cpuinfo  | grep processor | wc -l`
 load=`cat /proc/loadavg | awk -F. '{print$1}'`
@@ -19,7 +23,7 @@ up_time=`cat /proc/uptime | awk -F. '{print$1}'`
 if [[ $up_time -lt 86400 ]]
 then
 	print_warn "System was rebooted recently."
-	echo -ne "\n\t - "`uptime`
+	print_sub "`uptime`"
 fi
 ooms=`egrep "oom|Out of memory|out_of_memory" /var/log/messages | wc -l`
 if [[ $ooms -ge 1 ]]
@@ -27,33 +31,33 @@ then
 	print_warn "Found $ooms OOM events in /var/log/messages"
 	egrep "oom|Out of memory|out_of_memory" /var/log/messages | tail | while read line
 	do
-		echo -ne "\n\t - $line"
+		print_sub "$line"
 	done
 fi
 maxclients=`grep -i maxclients /var/log/httpd/error_log | wc -l`
 if [[ $maxclients -ge 1 ]]
 then
 	print_warn "Found $maxclients MaxClient hits in /var/log/httpd/error_log"
-	echo -ne "\n\t - "`grep -i maxclients /var/log/httpd/error_log | tail -1`
+	print_sub "`grep -i maxclients /var/log/httpd/error_log | tail -1`"
 fi
 if [[ `w | grep rack | wc -l` -gt 1 ]]
 then
 	print_warn "Another racker logged in."
 	pts=`tty | awk -F/ '{print$4}'`
 	otherpts=`w | grep rack | grep -v "pts/$pts" | awk '{print$2}' | awk -F/ '{print$2}'`
-	echo -ne "\t - "`w | grep rack | grep pts/$otherpts`
-	echo -ne "\n\t - "`grep racker /var/log/secure | grep "/dev/pts/$otherpts" | tail -1`
+	print_sub "`w | grep rack | grep pts/$otherpts`"
+	print_sub "`grep racker /var/log/secure | grep "/dev/pts/$otherpts" | tail -1`"
 fi
 if [[ `rpm -q psa | grep -v installed | wc -l` -ge 1 ]]
 then
 	print_warn "Plesk is installed."
-	echo -ne "\n\t - "`rpm -q psa`
-	echo -ne "\n\t - admin/"`if [[ $(rpm -q psa | awk -F"-" '{print$2}' | sed 's/\.//g') -le "1019" ]] ; then cat /etc/psa/.psa.shadow ; else /usr/local/psa/bin/admin --show-password ; fi`
+	print_sub "`rpm -q psa`"
+	print_sub "admin/`if [[ $(rpm -q psa | awk -F"-" '{print$2}' | sed 's/\.//g') -le "1019" ]] ; then cat /etc/psa/.psa.shadow ; else /usr/local/psa/bin/admin --show-password ; fi`"
 fi
 if [[ -f /usr/local/cpanel/version ]]
 then
 	print_warn "cPanel is installed."
-	echo -ne "\n\t - "`cat /usr/local/cpanel/version`
+	print_sub "`cat /usr/local/cpanel/version`"
 fi
 echo
 echo
