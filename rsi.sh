f82734cb98265affbb3c242a3568bbd953e790ff
Samir Jafferali <samir.jafferali@gmail.com>
2015-09-03 17:05:41 -0500

    Modified plesk link to use insecure link

--- rsi.sh
+++ rsi.sh
@@ -48,7 +48,7 @@ elif [[ `rpm -q psa | grep -v installed | wc -l` -ge 1 ]]
 then
 	print_info "Control Panel: Plesk (`rpm -q psa | awk -F"-" '{print$2}'`)"
 	print_sub "admin/`if [[ $(rpm -q psa | awk -F"-" '{print$2}' | sed 's/\.//g') -le "1019" ]] ; then cat /etc/psa/.psa.shadow ; else /usr/local/psa/bin/admin --show-password ; fi`"
-	print_sub "https://"$(curl -sk curlmyip.de)":8443/login_up.php3?login_name=admin&passwd="$(if [[ $(rpm -q psa | awk -F"-" '{print$2}' | sed 's/\.//g') -le "1019" ]] ; then cat /etc/psa/.psa.shadow ; else /usr/local/psa/bin/admin --show-password ; fi)
+	print_sub "http://"$(curl -sk curlmyip.de)":8880/login_up.php3?login_name=admin&passwd="$(if [[ $(rpm -q psa | awk -F"-" '{print$2}' | sed 's/\.//g') -le "1019" ]] ; then cat /etc/psa/.psa.shadow ; else /usr/local/psa/bin/admin --show-password ; fi)
 elif [[ -f /usr/local/cpanel/version ]]
 then
 	print_info "Control Panel: cPanel/WHM (`cat /usr/local/cpanel/version`)"
